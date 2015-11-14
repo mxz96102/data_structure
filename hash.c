b6584cfa351d4d49e45c60c4a03f7680cdedd395
mxz96102 <mxz96102@qq.com>
2015-11-14 13:08:42 +0800

    Update hash.c

--- hash.c
+++ hash.c
@@ -1,88 +1,170 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct index*in_head;
+void create_index();
+void create_node();
+void insert();
+struct index*head;
+int g_flag=0;
 
 struct index{
 	int key;
-	void *enter;
+	struct node*enter;
 	struct index*next;
 };
 
-struct hash_list{
+struct node{
 	int num;
-	int content;
-	struct hash_list*next;
+	struct node*next;
 };
 
-void mk_index(){
+void create_index(){
+	struct index*temp,*p1;
 	int n=0;
-	struct index*p1,*p2;
-	in_head=p1;
-	while (n>10){
-		p1->key=n;
-		p2=p1;
-		p1=(struct index*)malloc(sizeof(struct index));
-		if(p1==NULL)
-			printf("error");
-		p2->next=p1;
-		n++；
-	}
-}	
-
-void hash_input(){
-	struct index*i_p;
-	struct hash_list*h_p;
-	struct hash_list*temp;
-	int n=1;
-	while(n==1){
-		temp=(struct hash_list*)malloc(sizeof(struct hash_list));
-		scanf("%d %d",&temp->num,&temp->content);
-		if(temp==NULL){	
-			printf("error");
-			break;}
-		if((temp->num)==0)
-			break;
-		i_p=in_head;
-		while(i_p->next!=NULL){
-			if ((temp->num)%10==(i_p->key)){
-				if (i_p->enter==NULL)
-					(i_p->enter)=temp;
-				else {
-					h_p=(i_p->enter);
-					while(h_p->next!=NULL)
-						h_p=h_p->next;
-					h_p->next=temp;}
-				i_p=(i_p->next);
-			}
+	while(n<=10){
+		temp=(struct index*)malloc(sizeof(struct index));
+		if (temp==NULL){
+			printf("meerror\n");
+			exit(1);}
+		if (n==0){
+			head=temp;
+			p1=head;}
+		temp->key=n;
+		p1->next=temp;
+		p1=temp;
+		n++;
 		}
-	}
+	printf("index created completely\n");
 }
-				
-struct hash_list*find(int n){
-	int m;
-	struct index*i_p;
-	struct hash_list*h_p;
+
+void create_node(){
+	while(g_flag==0)
+		insert();
+	printf("hash created completely\n");
+}
+
+void insert(){
+	struct index*p;
+	struct node*temp,*p1,*p2;
+	int n,m,flag;
+	flag=scanf("%d",&n);
+	if(!flag){
+		printf("input error\n");
+		exit(1);}
+	if(flag==EOF)
+		g_flag=1;
+	temp=(struct node*)malloc(sizeof(struct node));
+	if (temp==NULL){
+		printf("meerror\n");
+		exit(1);}
+	temp->num=n;
+	p=head;
 	m=n%10;
-	i_p=in_head;
-	while(i_p->key!=m&&i_p->next!=NULL)
-		i_p=i_p->next;
-	h_p=(i_p->enter);
-	while(h_p->num!=n&&h_p->next!=NULL)
-		h_p=h_p->next;
-	return h_p;
+	while(m!=(p->key))
+		p=p->next;
+	if(p->enter==NULL)
+		p->enter=temp;
+	else {
+		p1=p->enter;
+		while(p1->next!=NULL)
+			p1=p1->next;
+		p1->next=temp;
+		}
 }
 
+struct node*find(int n){
+	struct node*p1;
+	struct index*p;
+	int m;
+	
+	m=n%10;
+	p=head;
+	while(m!=(p->key)&&p!=NULL)
+		p=p->next;
+	p1=p->enter;
+	if(p1==NULL||p==NULL){
+		printf("oop,we didn't find it in heap\n");
+	    return NULL;}
 
-int main(){
-	struct hash_list*find(int n);
-	struct hash_list*p;
+	while(n!=(p1->num)&&p1!=NULL)
+		p1=p1->next;
+	if(p1==NULL||p==NULL){
+        printf("oop,we didn't find it in heap\n");
+        return NULL;}
+
+	return p1;
+}
+
+void delete(struct node*p1){
+	struct node*p2;
+	struct index*p;
+	int m;
+	m=(p1->num)%10;
+	p=head;
+	while(m!=(p->key))
+		p=p->next;
+	if((p->enter)==p1&&(p1->next)==NULL)
+		p->enter=NULL;
+	else if(p->enter==p1)
+		p->enter=p1->next;
+	else {p2=p->enter;
+		while((p2->next)!=p1)
+			p2=p2->next;
+		p2->next=p1->next;}
+	free(p1);
+}
+
+void menu(){
+	struct node*p;
 	int n;
-	mk_index();
-	hash_input();
-	printf("ss\n");
+	printf("Hello!Can you hear me?....\n");
+	create_index();
+	printf("Well,let's input some integers to create your hash now\nwarning:if input is illegal,we have right to shut down the program.\n");
+	create_node();
+A:	printf("What can I didly do for you?\n\t1.insert data\n\t2.search the heap\n\t3.exit\n");
 	scanf("%d",&n);
-	p=find(n);
-	printf("%d",p->content);
+	switch(n){
+		case 1:
+			printf("please input a integers now\nwarning:if input is illegal,we have right to shut down the program.\n");
+			insert();
+			goto A;
+			break;
+		case 2:
+			printf("what num is you looking for?");
+			if(!scanf("%d",&n)){
+				printf("input error\n");
+			    exit(1);}
+			p=find(n);
+			if(p==NULL)
+				goto A;				
+			printf("we find:%d,what you want to do\n\t1.delete it\n\t2.exit\n",p->num);
+			scanf("%d",&n);
+			switch(n){
+				case 1:
+					printf("....ok\n");
+					delete(p);
+					goto A;
+					break;
+				case 2:
+					exit(1);
+					break;
+				default:
+					printf("i dont understand.\n");
+					exit(1);
+					break;
+				}
+			break;
+		case 3:
+			exit(1);
+			break;
+		default:
+			printf("i dont understand.\n");
+			exit(1);
+			break;
+	}	
+	
+}
+	
+int main(){
+	menu();
 	return 0;
 }
-
